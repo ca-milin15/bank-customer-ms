4767a92d35191098130fcd67183be1de91849d79
User Developer <backenddeveloper30@gmail.com>
2024-06-05 22:31:49 -0500

    Estructura inicial del proyecto

---
+++ src/test/java/com/pichincha/bank_customer_ms/BankCustomerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_customer_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
